2dffd61b726f14ef786b6c1c457dc9cbebba9bb0
Brunosccp <rocca_bruno@hotmail.com>
2018-08-18 11:43:58 -0300

    agora a comunidade vai em forma de esteira

--- Alba/GameScene.swift
+++ Alba/GameScene.swift
@@ -11,11 +11,11 @@ import GameplayKit
 
 struct PhysicsCatagory {
     static let pipaTeste : UInt32 = 0x1 << 1
-    static let testeCasa : UInt32 = 0x1 << 2
-    static let pipaRival : UInt32 = 0x1 << 3
+    static let comunidade : [UInt32] = [0x1 << 2, 0x1 << 3, 0x1 << 4]
+    static let pipaRival : UInt32 = 0x1 << 5
     
-    static let createTriggerCategory: UInt32 = 0x1 << 4
-    static let deleteTriggerCategory: UInt32 = 0x1 << 5
+    static let createTriggerCategory: UInt32 = 0x1 << 6
+    static let deleteTriggerCategory: UInt32 = 0x1 << 7
 }
 
 class GameScene: SKScene {
@@ -34,7 +34,8 @@ class GameScene: SKScene {
     private var path = UIBezierPath()
     
     var sky = SKSpriteNode()
-    var casasAlpha = SKSpriteNode()
+    var casasAlpha: [SKSpriteNode]! = []
+    var comunidade: [SKSpriteNode]! = []
     var pipa = SKSpriteNode()
     
     override func didMove(to view: SKView) {
@@ -54,42 +55,52 @@ class GameScene: SKScene {
         
         pipa.physicsBody = SKPhysicsBody(circleOfRadius: pipa.frame.height / 2)
         pipa.physicsBody?.categoryBitMask = PhysicsCatagory.pipaTeste
-        pipa.physicsBody?.collisionBitMask = PhysicsCatagory.testeCasa | PhysicsCatagory.pipaRival
-        pipa.physicsBody?.contactTestBitMask = PhysicsCatagory.testeCasa | PhysicsCatagory.pipaRival
+        pipa.physicsBody?.collisionBitMask = PhysicsCatagory.comunidade[0] | PhysicsCatagory.comunidade[1] | PhysicsCatagory.comunidade[2] | PhysicsCatagory.pipaRival
+        pipa.physicsBody?.contactTestBitMask = PhysicsCatagory.comunidade[0]|PhysicsCatagory.comunidade[1] | PhysicsCatagory.comunidade[2] | PhysicsCatagory.pipaRival
         pipa.physicsBody?.affectedByGravity = true
         pipa.physicsBody?.isDynamic = true
     }
     
     //função que cria as casas com o physics body em relação ao alpha da textura
     func createHouses(){
-        //pegando somente a textura da imagem
-        let casasTextura = SKTexture(imageNamed: "comunidade1")
+        var casasCircular: [SKSpriteNode] = []
+        
+        //pegando somente a textura das imagens
+        var comunidadeTextura : [SKTexture] = []
+        for i in 0...2{
+            comunidadeTextura.append(SKTexture(imageNamed: "comunidade\(i+1)"))
+        }
         
         //precisa-se criar uma node com física circular antes de criar a fisica pelo alpha da img
-        let casasCircular = SKSpriteNode(texture: casasTextura)
-        casasCircular.physicsBody = SKPhysicsBody(circleOfRadius: max(casasCircular.size.width / 2, casasCircular.size.height / 2))
-        casasCircular.setScale(0.3)
+        for i in 0...2{
+            casasCircular.append(SKSpriteNode(texture: comunidadeTextura[i]))
+            casasCircular[i].physicsBody = SKPhysicsBody(circleOfRadius: max(casasCircular[i].size.width / 2, casasCircular[i].size.height / 2))
+            casasCircular[i].setScale(0.3)
+        }
         
         //criando a física pelo alpha da textura
-        casasAlpha = childNode(withName: "comunidade1") as! SKSpriteNode
-        casasAlpha.physicsBody = SKPhysicsBody(texture: casasTextura,
-                                               size: CGSize(width: casasCircular.size.width,
-                                                            height: casasCircular.size.height))
+        for i in 0...2{
+        comunidade.append(childNode(withName: "comunidade\(i+1)") as! SKSpriteNode)
+        comunidade[i].physicsBody = SKPhysicsBody(texture: comunidadeTextura[i],
+                                               size: CGSize(width: casasCircular[i].size.width,
+                                                            height: casasCircular[i].size.height))
+        }
         
         //Fisica e Colisão
-        casasAlpha.physicsBody?.categoryBitMask = PhysicsCatagory.testeCasa
-        casasAlpha.physicsBody?.collisionBitMask = PhysicsCatagory.pipaTeste
-        casasAlpha.physicsBody?.contactTestBitMask = PhysicsCatagory.pipaTeste
-        casasAlpha.physicsBody?.affectedByGravity = false
-        casasAlpha.physicsBody?.isDynamic = false
-        casasAlpha.physicsBody?.pinned = false
+        for i in 0...2{
+            comunidade[i].physicsBody?.categoryBitMask = PhysicsCatagory.comunidade[i]
+            comunidade[i].physicsBody?.collisionBitMask = PhysicsCatagory.pipaTeste
+            comunidade[i].physicsBody?.contactTestBitMask = PhysicsCatagory.pipaTeste
+            comunidade[i].physicsBody?.affectedByGravity = false
+            comunidade[i].physicsBody?.isDynamic = false
+            comunidade[i].physicsBody?.pinned = false
+        }
         
-        let move = SKAction.follow(path.cgPath, asOffset: true, orientToPath: false, speed: 50)
-        casasAlpha.run(move)
+        for i in 0...2{
+            let move = SKAction.follow(path.cgPath, asOffset: true, orientToPath: false, speed: 50)
+            comunidade[i].run(move)
+        }
         
-        
-        //casasAlpha.zRotation = 90
-        //self.addChild(casasAlpha)
     }
     func createSky(){
         
