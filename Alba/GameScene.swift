ad3f36035c3044e40f606f6be3dc4ae32334203c
Brunosccp <rocca_bruno@hotmail.com>
2018-08-21 18:38:11 -0300

    arrumando fonte

--- Alba/GameScene.swift
+++ Alba/GameScene.swift
@@ -76,8 +76,8 @@ class GameScene: SKScene {
         scoreLabel = childNode(withName: "scoreLabel") as! SKLabelNode
         rivalCutLabel.text = "+50"
         rivalCutLabel.fontName = "Jua"
-        rivalCutLabel.color = .orange
-        rivalCutLabel.fontColor = .orange
+        rivalCutLabel.color = .white
+        rivalCutLabel.fontColor = .white
         
         
         scoreLabel.text = "Score: \(PhysicsCategory.score)"
